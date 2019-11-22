8bcb45ea704945232e47e42a8feaffc3929b8781
bilmakovchik <ruslan.bilmakovskyi@ekreative.com>
2019-11-22 20:41:53 +0200

    fixed wather count function

--- second/waterCount.js
+++ second/waterCount.js
@@ -1,53 +1,25 @@
-function waterAmount (arr) {
-  let secondMax = 0
-  let res = 0
+function waterAmount(arr) {
+  const n = arr.length;
 
-  while (arr.length > 0) {
-    let firstMax = arr[0]
-    let i = 0
-    i += 1
-    const waterArray = []
-    waterArray.push(firstMax)
-    if (firstMax <= arr[i]) {
-      firstMax = arr[i]
-      waterArray.shift()
-      arr.shift()
-      waterArray.push(firstMax)
-    } else {
-      for (let j = i; j < arr.length; j++) {
-        if (arr[j] >= firstMax) {
-          secondMax = arr[j]
-          waterArray.push(secondMax)
-          break
-        }
-        if (firstMax === Math.max.apply(null, arr)) {
-          arr.reverse()
-          i = 0
-          break
-        }
-        waterArray.push(arr[j])
-        i += 1
-      }
-      arr.splice(0, i)
-    }
-    res += countWater(waterArray)
-  }
-  return res
-}
+  let directArr = new Array(n);
+  let reverseArr = new Array(n);
+  let water = 0;
 
-function countWater (arr) {
-  let start = arr[0]
-  let res = 0
-  if (arr[0] < arr[arr.length - 1]) {
-    start = arr[0]
-  } else {
-    arr.reverse()
-    start = arr[0]
+  directArr[0] = arr[0];
+  for (let i = 1; i < n; ++i) {
+    directArr[i] = Math.max(directArr[i - 1], arr[i]);
   }
-  for (let i = 1; i < arr.length - 1; i++) {
-    res += start - arr[i]
+
+  reverseArr[n - 1] = arr[n - 1];
+  for (let i = n - 2; i >= 0; --i) {
+    reverseArr[i] = Math.max(reverseArr[i + 1], arr[i]);
   }
-  return res
+
+  arr.forEach((item, index) => {
+    water += Math.min(directArr[index], reverseArr[index]) - item;
+  });
+
+  return water;
 }
 
 console.log(waterAmount([2, 5, 1, 3, 1, 2, 1, 7, 7, 6])) // 17
